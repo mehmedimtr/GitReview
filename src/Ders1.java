3f23f3c64e987623aed1cd0994e8b3bb41e1cad2
mehmet <mydespero@gmail.com>
2020-09-29 13:29:41 -0300

    4. satir

--- src/Ders1.java
+++ src/Ders1.java
@@ -4,6 +4,7 @@ public class Ders1 {
 		System.out.println("Saat 11:53");
 		System.out.println("Saat 12:30");
 		System.out.println("Saat 19:17 - iskender");
+		System.out.println("Saat 13:24 - mehmet cincflict");
 	}
 
 }
