3cafbf3a372ebbcd37388f6a2c5ce274993572a3
mehmet <mydespero@gmail.com>
2020-09-29 13:34:52 -0300

    son version

--- src/Ders1.java
+++ src/Ders1.java
@@ -3,7 +3,7 @@ public class Ders1 {
 	public static void main(String[] args) {
 		System.out.println("Saat 11:53");
 		System.out.println("Saat 12:30");
-		System.out.println("Saat 19:17 - iskender");
+		System.out.println("Saat 19:17 - Mehmet");
 		System.out.println("Saat 13:24 - mehmet cincflict");
 	}
 
