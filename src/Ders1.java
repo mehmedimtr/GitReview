9c3ca82e5f69ba96c678fd8b53e6dea50c4d6bfe
mehmet <mydespero@gmail.com>
2020-09-30 12:13:21 -0300

    feature 2. satir

--- src/Ders1.java
+++ src/Ders1.java
@@ -3,9 +3,11 @@ public class Ders1 {
 	public static void main(String[] args) {
 		System.out.println("Saat 11:53");
 		System.out.println("Saat 12:30");
-		System.out.println("Saat 19:17 - Mehmet");
-		
+		System.out.println("Saat 19:17 - Mehmet");		
 		System.out.println("Saat 13:24 - mehmet cincflict");
+		System.out.println("Saat 12:05 - feature branch 1. satir");
+		System.out.println("Saat 12:12 - feature branch 2. satir");
+		
 	}
 
 }
